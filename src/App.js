0b381e11ff5c89e3988b5ca05af7ec82be672758
7hourspg <rajiv840704@gmail.com>
2023-02-24 00:53:30 +0530

    Todo_work

--- src/App.js
+++ src/App.js
@@ -1,50 +1,38 @@
-import React, {useState, useEffect} from "react";
-import Child from "./Child";
+import React, {useState} from "react";
+import arr from "./DummyData";
+import "./App.css";
 
 function App() {
-  const [state, setState] = useState(20);
-  const [name, setName] = useState("");
+  const [data, setData] = useState(arr);
+  const [input, setInput] = useState("");
 
-  const handleCLick = () => {
-    setState((prev) => prev + 1);
+  const handleInput = (e) => {
+    setInput(e.target.value);
   };
-  const handleName = () => {
-    setName("Priyesh");
+  const hanldleSubmit = () => {
+    let id = data.length + 1;
+    setData([...data, {id: id, title: input}]);
   };
-
-  // setTimeout(() => {
-  //   setState(state + 10);
-  // }, 2000);
-  // setInterval
-
-  // let stop = setInterval(() => {
-  //   console.log("Hello");
-  // }, 2000);
-  // const handleStop = () => {
-  //   clearInterval(stop);
-  // };
-
-  // useEffect(() => {
-  //   console.log("Hello");
-  // }, [name]);
-  const hanleChage = (e) => {
-    setName(e.target.value);
-  };
-  console.log(name);
-
-  let arr = [
-    {
-      name: "rajiv",
-      roll: 20,
-    },
-  ];
-
-  console.log(...arr);
   return (
-    <div>
-      <input type="text" onChange={hanleChage} />
-      <h1>{name}</h1>
-    </div>
+    <>
+      <div className="container">
+        <div className="inputBx">
+          <input type="text" placeholder="enter text" onChange={handleInput} />
+          <button onClick={hanldleSubmit} className="btn">
+            Submit
+          </button>
+        </div>
+        <div className="innerContainer">
+          {data?.map((item) => {
+            return (
+              <div key={item.id} className="wrapper">
+                <p>{item.title}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
   );
 }
 
