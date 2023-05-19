6bc25aae0d7181a4c2f99ff8d50a8a51637f3611
7hourspg <rajiv840704@gmail.com>
2023-05-19 19:18:36 +0530

    Trying

--- src/App.js
+++ src/App.js
@@ -2,50 +2,50 @@ import React, {useState, useEffect} from "react";
 import Child from "./Child";
 
 function App() {
-  const [state, setState] = useState(20);
-  const [name, setName] = useState("");
+     const [state, setState] = useState(20);
+     const [name, setName] = useState("");
 
-  const handleCLick = () => {
-    setState((prev) => prev + 1);
-  };
-  const handleName = () => {
-    setName("Priyesh");
-  };
+     const handleCLick = () => {
+          setState((prev) => prev + 1);
+     };
+     const handleName = () => {
+          setName("Priyesh");
+     };
 
-  // setTimeout(() => {
-  //   setState(state + 10);
-  // }, 2000);
-  // setInterval
+     // setTimeout(() => {
+     //   setState(state + 10);
+     // }, 2000);
+     // setInterval
 
-  // let stop = setInterval(() => {
-  //   console.log("Hello");
-  // }, 2000);
-  // const handleStop = () => {
-  //   clearInterval(stop);
-  // };
+     // let stop = setInterval(() => {
+     //   console.log("Hello");
+     // }, 2000);
+     // const handleStop = () => {
+     //   clearInterval(stop);
+     // };
 
-  // useEffect(() => {
-  //   console.log("Hello");
-  // }, [name]);
-  const hanleChage = (e) => {
-    setName(e.target.value);
-  };
-  console.log(name);
+     // useEffect(() => {
+     //   console.log("Hello");
+     // }, [name]);
+     const hanleChage = (e) => {
+          setName(e.target.value);
+     };
+     console.log(name);
 
-  let arr = [
-    {
-      name: "rajiv",
-      roll: 20,
-    },
-  ];
+     let arr = [
+          {
+               name: "rajiv",
+               roll: 20,
+          },
+     ];
 
-  console.log(...arr);
-  return (
-    <div>
-      <input type="text" onChange={hanleChage} />
-      <h1>{name}</h1>
-    </div>
-  );
+     console.log(...arr);
+     return (
+          <div>
+               <input type="text" onChange={hanleChage} />
+               <h1>{name}</h1>
+          </div>
+     );
 }
 
 export default App;
